0b1aaaafc0fe219b82f2fcafafad98526ef85b8b
Emmanuel Praise <emmapraise@gmail.com>
2020-08-21 16:33:10 +0100

    Issues fixed

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template,url_for,request
+from flask import Flask, render_template,request
 import pandas as pd
-import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
@@ -48,4 +47,4 @@ def predict():
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
